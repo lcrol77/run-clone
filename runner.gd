526ae7e490134bf51f28081572bd05428d26068c
Logan Croley <logan.croley.18@gmail.com>
2026-01-24 15:15:09 -0500

    refact

--- runner.gd
+++ runner.gd
@@ -8,19 +8,15 @@ extends CharacterBody3D
 @export var jump_velocity := 4.5
 @export var max_tilt_deg := 30.0
 @export var tilt_speed := 8.0
+@export var rotate_cooldown := 0.15
 
 @onready var skin: SkinController = $Skin
 @onready var right_ray: RayCast3D = $RightRay
 @onready var left_ray: RayCast3D = $LeftRay
 
-
-@export var rotate_cooldown := 0.15
 var rotate_lock := 0.0
-
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-var gravity_vector := Vector3(0,-1,0)
 var jump_buffered := false
-
 var just_transitioned: bool = false
 
 func _ready() -> void:
@@ -40,26 +36,37 @@ func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity += (-up_direction) * gravity * delta
-	# Jump (buffered)
-	if jump_buffered and is_on_floor():
-		velocity.y = jump_velocity
-	jump_buffered = false
-
+	_handle_jump()
 	# Horizontal control (x)
 	var axis := Input.get_axis(&"move_left", &"move_right")
-	var target_x := axis * move_speed
-	velocity.x += target_x
-	velocity.x *= exp(-ground_damping * delta)
-	# Constant forward
-	velocity.z = -forward_speed
+	_handle_movement(axis, delta)
+	_handle_skin_rotation(axis, delta)
+	_handle_wall_rotation(axis)
+	move_and_slide()
+
+func _rotate_90(s: int) -> void:
+	var angle := deg_to_rad(90.0 * s)
+	var axis_world := (-global_transform.basis.z).normalized()
+	var new_up := up_direction.rotated(axis_world, angle).normalized()
+	global_rotate(axis_world, angle)
+	velocity = velocity.rotated(axis_world, angle)
+	up_direction = new_up
+
+func  _handle_jump() -> void:
+		if jump_buffered and is_on_floor():
+			velocity.y = jump_velocity
+		jump_buffered = false
 	
-	# skin rotation stuff
-	var target_yaw := -deg_to_rad(axis * max_tilt_deg)
-	skin.rotation.y = lerp_angle(
-		skin.rotation.y,
-		target_yaw,
-		tilt_speed * delta
-	)
+
+func _handle_skin_rotation(axis: float, delta:float)->void:
+		var target_yaw := -deg_to_rad(axis * max_tilt_deg)
+		skin.rotation.y = lerp_angle(
+			skin.rotation.y,
+			target_yaw,
+			tilt_speed * delta
+		)
+
+func _handle_wall_rotation(axis) -> void:
 	if rotate_lock <= 0.0:
 		if axis > 0.0 and right_ray.is_colliding():
 			_rotate_90(-1) # right turn
@@ -67,26 +74,13 @@ func _physics_process(delta: float) -> void:
 		elif axis < 0.0 and left_ray.is_colliding():
 			_rotate_90(1)  # left turn
 			rotate_lock = rotate_cooldown
-		
-	move_and_slide()
 
-func _rotate_90(sign: int) -> void:
-	var angle := deg_to_rad(90.0 * sign)
-
-	# Use the runner's forward axis in WORLD space.
-	# If you move forward with velocity.z = -forward_speed,
-	# then "forward" is -basis.z.
-	var axis_world := (-global_transform.basis.z).normalized()
-
-	# Compute the new up BEFORE rotating the transform
-	var new_up := up_direction.rotated(axis_world, angle).normalized()
-
-	# Rotate the body around the lane axis (world-space)
-	global_rotate(axis_world, angle)
-
-	# Rotate velocity to match the new orientation (important!)
-	velocity = velocity.rotated(axis_world, angle)
-
-	# Update up + gravity
-	up_direction = new_up
-	gravity_vector = -up_direction
+# The problem here is that I need to selectively change the vector I am writing two
+# because I am sliding in the +/- X when  my up_direction is (0,1,0) and +/- Y 
+# when it is (1,0,0)
+func _handle_movement(axis: float, delta:float) -> void:
+		var target := axis * move_speed
+		velocity.x += target
+		velocity.x *= exp(-ground_damping * delta)
+		# Constant forward
+		velocity.z = -forward_speed
