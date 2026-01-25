2615973adafe56d96f57afacf15960da1de469d7
Logan Croley <logan.croley.18@gmail.com>
2026-01-25 13:49:46 -0500

    refactor

--- runner.gd
+++ runner.gd
@@ -14,87 +14,140 @@ extends CharacterBody3D
 @onready var right_ray: RayCast3D = $RightRay
 @onready var left_ray: RayCast3D = $LeftRay
 
+enum AxisIndex { X, Y }
+
 var rotate_lock := 0.0
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var jump_buffered := false
-var just_transitioned: bool = false
 
 func _ready() -> void:
 	skin.set_animation("walk")
 
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed(&"reset_position"):
-		global_position = Vector3(0, 1, 0)
-		velocity = Vector3.ZERO
-		reset_physics_interpolation()
+		_reset_runner()
 
 	if Input.is_action_just_pressed(&"jump"):
 		jump_buffered = true
 
+
 func _physics_process(delta: float) -> void:
-	rotate_lock = max(0.0, rotate_lock - delta)
-	# Gravity
-	if not is_on_floor():
-		velocity += (-up_direction) * gravity * delta
+	rotate_lock = maxf(0.0, rotate_lock - delta)
+
+	_apply_gravity(delta)
 	_handle_jump()
-	# Horizontal control (x)
-	var axis := Input.get_axis(&"move_left", &"move_right")
-	_handle_movement(axis, delta)
-	_handle_skin_rotation(axis, delta)
-	_handle_wall_rotation(axis)
+
+	var input_axis := Input.get_axis(&"move_left", &"move_right")
+	_apply_lateral(input_axis, delta)
+	_apply_skin_tilt(input_axis, delta)
+	_try_wall_rotate(input_axis)
+
 	move_and_slide()
+
+
+# ----------------------------
+# Movement pieces
+# ----------------------------
+
+func _apply_gravity(delta: float) -> void:
+	if not is_on_floor():
+		# same as your original: velocity += (-up_direction) * gravity * delta
+		velocity += (-up_direction) * gravity * delta
+
+
+func _handle_jump() -> void:
+	if jump_buffered and is_on_floor():
+		velocity.y = jump_velocity
+	jump_buffered = false
+
+
+func _apply_forward() -> void:
+	velocity.z = -forward_speed
+
+
+func _apply_lateral(input_axis: float, delta: float) -> void:
+	var cfg := _lateral_config()
+	var target : float = input_axis * move_speed * cfg.sign
+
+	# mutate the correct component
+	match cfg.axis:
+		AxisIndex.X:
+			velocity.x += target
+			velocity.x *= exp(-ground_damping * delta)
+		AxisIndex.Y:
+			velocity.y += target
+			velocity.y *= exp(-ground_damping * delta)
+
+	_apply_forward()
+
+
+func _apply_skin_tilt(input_axis: float, delta: float) -> void:
+	var target_yaw := -deg_to_rad(input_axis * max_tilt_deg)
+	skin.rotation.y = lerp_angle(skin.rotation.y, target_yaw, tilt_speed * delta)
+
+
+# ----------------------------
+# Rotation pieces
+# ----------------------------
+
+func _try_wall_rotate(input_axis: float) -> void:
+	if rotate_lock > 0.0:
+		return
+
+	if input_axis > 0.0 and right_ray.is_colliding():
+		_rotate_90(-1) # right
+		rotate_lock = rotate_cooldown
+	elif input_axis < 0.0 and left_ray.is_colliding():
+		_rotate_90(1)  # left
+		rotate_lock = rotate_cooldown
+
 
 func _rotate_90(s: int) -> void:
 	var angle := deg_to_rad(90.0 * s)
 	var axis_world := (-global_transform.basis.z).normalized()
+
 	var new_up := up_direction.rotated(axis_world, angle).normalized()
 	global_rotate(axis_world, angle)
+
 	velocity = velocity.rotated(axis_world, angle)
 	up_direction = new_up
 
-func  _handle_jump() -> void:
-	if jump_buffered and is_on_floor():
-		velocity.y = jump_velocity
-	jump_buffered = false
-	
 
-func _handle_skin_rotation(axis: float, delta:float)->void:
-	var target_yaw := -deg_to_rad(axis * max_tilt_deg)
-	skin.rotation.y = lerp_angle(
-		skin.rotation.y,
-		target_yaw,
-		tilt_speed * delta
-	)
+# ----------------------------
+# Orientation logic (centralized)
+# ----------------------------
 
-func _handle_wall_rotation(axis) -> void:
-	if rotate_lock <= 0.0:
-		if axis > 0.0 and right_ray.is_colliding():
-			_rotate_90(-1) # right turn
-			rotate_lock = rotate_cooldown
-		elif axis < 0.0 and left_ray.is_colliding():
-			_rotate_90(1)  # left turn
-			rotate_lock = rotate_cooldown
+# Returns:
+# - axis: which velocity component we treat as "lateral"
+# - sign: multiplier for inversion
+func _lateral_config() -> Dictionary:
+	# Use dot checks instead of exact float equality.
+	# "Up is mostly X" means we are on a wall where up_direction points ±X.
+	var up_x := up_direction.dot(Vector3.RIGHT)  # +1 if (1,0,0), -1 if (-1,0,0)
+	var up_y := up_direction.dot(Vector3.UP)     # +1 if (0,1,0), -1 if (0,-1,0)
 
-# The problem here is that I need to selectively change the vector I am writing two
-# because I am sliding in the +/- X when  my up_direction is (0,1,0) and +/- Y 
-# when it is (1,0,0)
-func _handle_movement(axis: float, delta:float) -> void:
-	var axis_of_movement := _get_current_axis_of_movement()
-	var target := axis * move_speed * _is_inverted_multiplier()
-	velocity[axis_of_movement] += target
-	velocity[axis_of_movement] *= exp(-ground_damping * delta)
-	# Constant forward
-	velocity.z = -forward_speed
+	var axis := AxisIndex.X
+	if absf(up_x) > 0.5:
+		axis = AxisIndex.Y
+	else:
+		axis = AxisIndex.X
 
-func _get_current_axis_of_movement() -> String:
-	if up_direction.x == 1 or up_direction.x == -1:
-		return "y"
-	if up_direction.y == 1 or up_direction.y == -1:
-		return "x" 
-	return "x"
+	# Preserve your intent: sometimes left/right should invert depending on orientation.
+	# Your original condition was: if up_direction.x == 1 or up_direction.y == -1 => -1 else +1.
+	# We'll express the same idea with dot products.
+	var s := 1
+	if up_x > 0.5 or up_y < -0.5:
+		s = -1
 
-func _is_inverted_multiplier() -> int:
-	print(up_direction)
-	if up_direction.x == 1 or up_direction.y == -1:
-		return -1
-	return 1
+	return { "axis": axis, "sign": s }
+
+
+# ----------------------------
+# Misc
+# ----------------------------
+
+func _reset_runner() -> void:
+	global_position = Vector3(0, 1, 0)
+	velocity = Vector3.ZERO
+	reset_physics_interpolation()
